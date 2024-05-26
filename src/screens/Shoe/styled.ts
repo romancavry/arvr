d25c2d7003ddc0c8c56bc1c65b657a7826f59a24
Roman Antipov <rantipov@atomation.ru>
2024-05-26 16:45:28 +0300

    feat: add styles

--- src/screens/Shoe/styled.ts
+++ src/screens/Shoe/styled.ts
@@ -1,6 +1,10 @@
 import { styled } from '@linaria/react';
 
+import { COLOR, TEXT } from 'uikit/vars';
+
 export const Wrap = styled.div`
+  position: relative;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -14,8 +18,21 @@ export const Wrap = styled.div`
     position: absolute !important;
     top: 74px;
     left: 70px;
+
     width: 90px !important;
     height: 90px !important;
+  }
+
+  h1 {
+    position: absolute;
+    top: 43px;
+    left: 140px;
+
+    margin: 40px;
+    padding: 0;
+
+    ${TEXT.H1};
+    color: var(${COLOR.MAIN});
   }
 `;
 
